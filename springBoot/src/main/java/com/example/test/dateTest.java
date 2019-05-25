f1b95c67f9159dd1adb795b7aa1ba9483600fc72
wanghognjie <wanghongjie@ysstech.com>
2019-05-25 17:14:13 +0800

    生成日志

--- springBoot/src/main/java/com/example/test/dateTest.java
+++ springBoot/src/main/java/com/example/test/dateTest.java
@@ -318,33 +318,7 @@ public class dateTest {
         System.out.println(char1.trim());
     }
 
-    public static String md5Encode(String inStr) throws Exception {
-        MessageDigest md5 = null;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            e.printStackTrace();
-            return "";
-        }
 
-        byte[] byteArray = inStr.getBytes("UTF-8");
-        byte[] md5Bytes = md5.digest(byteArray);
-        StringBuffer hexValue = new StringBuffer();
-        for (int i = 0; i < md5Bytes.length; i++) {
-            int val = ((int) md5Bytes[i]) & 0xff;
-            if (val < 16) {
-                hexValue.append("0");
-            }
-            hexValue.append(Integer.toHexString(val));
-        }
-        return hexValue.toString();
-    }
-    @Test
-    public void test111() throws Exception {
-        String md5Encode = this.md5Encode("123456888");
-        System.out.println("123456加密后="+md5Encode);
-    }
     public static final String PKCS12 = "PKCS12";
     public static final String JKS = "JKS";
     public static final String PFX_KEYSTORE_FILE = "D://pyas.pfx";
